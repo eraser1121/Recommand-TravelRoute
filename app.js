ae6b9301e43c0a3ab56bb6dd81bfcec59c954fd1
eraser <eraser1121@khu.ac.kr>
2020-11-23 23:00:16 +0900

    Kakao Maps API 연동

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var mainRouter = require('./routes/main');
+var searchRouter = require('./routes/search')
 var app = express();
 
 // view engine setup
@@ -19,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', mainRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
